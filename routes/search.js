00f4109ec67ba0e4706b440f5e037e57f3447618
Noel Anstey <anstey.ng@gmail.com>
2017-03-31 15:21:19 -0700

    mad troubleshooting. Submit insert query gets down to search.js

--- routes/search.js
+++ routes/search.js
@@ -1,39 +1,50 @@
+var request = require('request');
+var jsdom = require('jsdom');
+var $ = require('jquery')(jsdom.jsdom().defaultView);
 
-function catAss(uberString) {
-  console.log(uberString);
+function catAss(linkList, cb) {
+  // console.log(uberString);
   let cat_id = 4;
-  if (uberString.includes('movie' || 'tv' || 'show' || 'film' || 'series')) {
-    cat_id = 1;
-  } else if (uberString.includes('paper' || 'author' || 'read' || 'novel' || 'books')) {
-    cat_id = 2;
-  } else if (uberString.includes('food' || 'eat' || 'drink' || 'restaurant' || 'cafe')) {
-    cat_id = 3;
-  } else if (uberString.includes('music' || 'band' || 'artist' || 'song' || 'concert')) {
-    cat_id = 5;
-  }
-  console.log('After if statement cat_it is: ', cat_id);
-  return cat_id;
+  // if (uberString.includes('movie' || 'tv' || 'show' || 'film' || 'series')) {
+  //   cat_id = 1;
+  // } else if (uberString.includes('paper' || 'author' || 'read' || 'novel' || 'books')) {
+  //   cat_id = 2;
+  // } else if (uberString.includes('food' || 'eat' || 'drink' || 'restaurant' || 'cafe')) {
+  //   cat_id = 3;
+  // } else if (uberString.includes('music' || 'band' || 'artist' || 'song' || 'concert')) {
+  //   cat_id = 5;
+  // }
+  // console.log('After if statement cat_id is: ', cat_id);
+  cb( cat_id );
 }
 
 module.exports = {
-  listQuery: function(itemQuery){
+  listQuery: function(itemQuery, cb){
+    // console.log('itemQuery: ', itemQuery);
     let searchString = itemQuery.toLowerCase();
-    let HTMLstring = itemQuery.replace(" ", "_");
+    let HTMLstring = itemQuery.replace(" ", "+");
     console.log('searchString', searchString, 'HTMLstring', HTMLstring);
     let linkList = [];
-    $(() => {
-      $.ajax({
-        method: "GET",
-        url:`https://www.googleapis.com/customsearch/v1?key=AIzaSyAdslr-npcuLlN7_7-QmRV8wnVVHjgGKJ4&cx=011814553479746519374:at46p1fcles&q=${itemQuery}&callback=hndlr`
-      }).done( (response) => {
-        for (var i = 0; i < response.items.length; i++) {
-          linkList.push(item.link);
-          searchString += (' ' + item.snippet + ' ' + item.title + ' ');
-        }
+    let url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyAdslr-npcuLlN7_7-QmRV8wnVVHjgGKJ4&cx=011814553479746519374:at46p1fcles&q=${itemQuery}`
 
-        let retObj = {'cat_id': catAss(searchString), 'link': linkList[0]};
-        return retObj;
+
+    $.ajax({
+      method: "GET",
+      url: url,
+    }).done( (response) => {
+      // console.log(response.items)/**/;
+      let items = response.items;
+      for (var item of items) {
+        // console.log(item.link);
+        linkList.push(item.link);
+      }
+      catAss(linkList, (id) =>{
+        let retObj = {'cat_id': id, 'link': linkList[0] };
+        console.log(retObj)
+        cb(retObj);
       });
+
     });
+
   }
 }
