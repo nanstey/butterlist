15e49ce14eb7d8e7c724bbb05d2d90265cd68cc5
Wyatt Robinson <thewyatt@gmail.com>
2017-03-31 01:27:49 +0000

    Added catAss function to search.js

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,20 @@
-
+function catAss(uberString) {
+  let cat_id = 4;
+  if (uberString.includes('movie' || 'tv' || 'show' || 'film' || 'series')) {
+    cat_id = 1;
+  } else if (uberString.includes('paper' || 'author' || 'read' || 'novel' || 'books')) {
+    cat_id = 2;
+  } else if (uberString.includes('food' || 'eat' || 'drink' || 'restaurant' || 'cafe')) {
+    cat_id = 3;
+  } else if (uberString.includes('music' || 'band' || 'artist' || 'song' || 'concert')) {
+    cat_id = 5;
+  }
+  return cat_id;
+}
 
 module.exports = {
-  listQuery: function(params){
-    let itemQuery = params;
+  listQuery: function(itemQuery){
+    let searchString = query.toLowerCase();
     let linkList = [];
     $(() => {
       $.ajax({
@@ -11,7 +23,9 @@ module.exports = {
       }).done((response) => {
           for (var i = 0; i < response.items.length; i++) {
             linkList.push(item.link);
+            searchString += (' ' + item.snippet + ' ' + item.title + ' ');
           }
+
           let retObj = {'cat_id':1, 'link': linkList[0]};
           return retObj;
         }
