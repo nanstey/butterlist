b4ee78bd9e89f59cab054a3f1da455af0960391c
Noel Anstey <anstey.ng@gmail.com>
2017-03-30 18:32:00 -0700

    finished api/search

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,16 @@ const router  = express.Router();
 
 module.exports = (DataHelpers) => {
   router.post("/search", (req, res) => {
-    let userSearch = search.listQuery(data);
-
+    if (req.session.user_id){
+      let id = req.session.user_id;
+      let query = req.body.inputQuery;
+      let result = search.listQuery(query);
+      DataHelpers(id, result.cat_id, query, result.link, (item) => {
+        res.status(201).send(item);
+      })
+    } else {
+      res.status(400).send('Bad Request');
+    }
   });
 
   router.get("/lists", (req, res) => {
@@ -20,4 +28,3 @@ module.exports = (DataHelpers) => {
   return router;
 }
 
-//This
