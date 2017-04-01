b5b5bd41393e5080e13e5515b2b3b0b96e7c1bc0
Wyatt Robinson <thewyatt@gmail.com>
2017-04-01 00:50:39 +0000

    catAss function with basic ability to set category after first match to any of our keywords.

--- catAss.js
+++ catAss.js
@@ -1,34 +1,21 @@
-let testArray = [
-  'https://en.wikipedia.org/wiki/Cannibal_Corpse'
-  'http://cannibalcorpse.net/',
-  'https://www.discogs.com/artist/124774-Cannibal-Corpse',
-  'https://www.youtube.com/watch?v=XAIX2vISe3M',
-  'http://www.imdb.com/name/nm2472483/',
-  'https://www.facebook.com/cannibalcorpse/',
-  'http://www.imdb.com/title/tt1849737/',
-  'https://twitter.com/corpseofficial?lang=en',
-  'http://www.allmusic.com/artist/cannibal-corpse-mn0000545534',
-  'https://www.last.fm/music/Cannibal+Corpse'
-  ];
+const triggerWords = [
+                  ['tv','movie','theatre','series','film','actor']
+                  ['book','indigo','chapters','novel','read'],
+                  ['food','yelp','tripadvisor','drink','pizza','beer'],
+                  ['products','amazon','walmart'],
+                  ['music','discogs','artist','allmusic'],
+                  ];
 
-function extractDomain(url) {
-    var domain;
-    //find & remove protocol (http, ftp, etc.) and get domain
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    }
-    else {
-        domain = url.split('/')[0];
+function catAss(array, keywords) {
+  let cat_id = 0;
+  for(var i =0; i < array.length;i++) {
+    for (var j=0; j < keywords.length; j++) {
+      for (var k=0; k< keywords[j].length; k++) {
+        if (array[i].includes(keywords[j][k])){
+          return cat_id = j + 1;
+        }
+      }
     }
 
-    //find & remove port number
-    domain = domain.split(':')[0];
-
-    return domain;
+  }
 }
-
-console.log(extractDomain);
-
-  // function catAss(array) {
-
-  // }
