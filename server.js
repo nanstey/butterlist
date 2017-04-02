abb448a271cd48c3f02531110a4f3af7bf65ea0d
Noel Anstey <anstey.ng@gmail.com>
2017-04-01 17:09:15 -0700

    removed userRoutes

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const knexLogger  = require('knex-logger');
 
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
 const DataHelpers = require("./db/dataHelpers.js")(knex);
 const routes = require("./routes/routes")(DataHelpers);
 const api = require("./routes/api")(DataHelpers);
