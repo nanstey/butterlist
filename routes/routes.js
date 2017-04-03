1a1fdcc49ccfb63bceb9f2099a08e6eb0700a6dc
Noel Anstey <anstey.ng@gmail.com>
2017-04-02 18:58:39 -0700

    new handler for post(/login)

--- routes/routes.js
+++ routes/routes.js
@@ -42,72 +42,82 @@ module.exports = function(DataHelpers) {
 
   //Registering
   router.post('/register', (req, res) => {
-    console.log('REQ',req.body);
-    if(!req.body.email || !req.body.password) {
-      res.redirect('/login');
-      // res.status(400).redirect('https://http.cat/400');
-      return;
-    }
-    const findRequestedEmail = knex('users')
-      .select(1)
-      .where({email: req.body.email});
-    findRequestedEmail.then((rows) => {
-      if(rows.length) {
-        return Promise.reject({
-          type: 409,
-          message: 'email has already been used.'
-        });
-      }
-     return bcrypt.hash(req.body.password, 10);
-    }).then((passwordResponse) => {
-      return knex('users').insert({
-        name: req.body.name,
-        email: req.body.email,
-        password: passwordResponse
-      }, 'id');
-    }).then((user_id) => {
-      // req.flash('info', 'account successfully created');
-      req.session.user_id = user_id[0];
-      res.redirect('/');
-    }).catch((err) => {
-      // req.flash('errors', err.message);
-      res.redirect('/');
-    });
+    // console.log('REQ',req.body);
+    // if(!req.body.email || !req.body.password) {
+    //   res.redirect('/login');
+    //   // res.status(400).redirect('https://http.cat/400');
+    //   return;
+    // }
+    // const findRequestedEmail = knex('users')
+    //   .select(1)
+    //   .where({email: req.body.email});
+    // findRequestedEmail.then((rows) => {
+    //   if(rows.length) {
+    //     return Promise.reject({
+    //       type: 409,
+    //       message: 'email has already been used.'
+    //     });
+    //   }
+    //  return bcrypt.hash(req.body.password, 10);
+    // }).then((passwordResponse) => {
+    //   return knex('users').insert({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: passwordResponse
+    //   }, 'id');
+    // }).then((user_id) => {
+    //   // req.flash('info', 'account successfully created');
+    //   req.session.user_id = user_id[0];
+    //   res.redirect('/');
+    // }).catch((err) => {
+    //   // req.flash('errors', err.message);
+    //   res.redirect('/');
+    // });
   });
 
   // Login handler POST
   router.post('/login', (req, res) => {
-    console.log(req.body.password);
-    const findUserByEmail = knex('users')
-      .select('id', 'password')
-      .where({email: req.body.email})
-      .limit(1);
-    findUserByEmail.then((rows) => {
-      const user = rows[0];
-      if(!user) {
-        return Promise.reject({
-          type: 409,
-          message: 'Bad credentials.'
-        });
-      }
-      const comparePasswords = bcrypt.compare(req.body.password, user.password);
-      return comparePasswords.then((passwordsMatch) => {
-        if(!passwordsMatch) {
-          return Promise.reject({
-            type: 409,
-            message: 'no'
-          });
-        }
-        return Promise.resolve(user);
+    if (req.session.user_id){
+      res.redirect('/');
+    } else {
+      DataHelpers.validateEmailPassword(req.body.email, req.body.password, (id) => {
+        (id) ? req.session.user_id = id : req.flash('errors', 'Bad credentials');
+        res.redirect('/');
       });
-    }).then((user) => {
-      req.session.user_id = user.id;
-      res.redirect('/');
-    }).catch((err) => {
-      req.flash('errors', err.message);
-      res.redirect('/');
-    });
+    }
   });
+
+  //   console.log(req.body.password);
+  //   const findUserByEmail = knex('users')
+  //     .select('id', 'password')
+  //     .where({email: req.body.email})
+  //     .limit(1);
+  //   findUserByEmail.then((rows) => {
+  //     const user = rows[0];
+  //     if(!user) {
+  //       return Promise.reject({
+  //         type: 409,
+  //         message: 'Bad credentials.'
+  //       });
+  //     }
+  //     const comparePasswords = bcrypt.compare(req.body.password, user.password);
+  //     return comparePasswords.then((passwordsMatch) => {
+  //       if(!passwordsMatch) {
+  //         return Promise.reject({
+  //           type: 409,
+  //           message: 'no'
+  //         });
+  //       }
+  //       return Promise.resolve(user);
+  //     });
+  //   }).then((user) => {
+  //     req.session.user_id = user.id;
+  //     res.redirect('/');
+  //   }).catch((err) => {
+  //     req.flash('errors', err.message);
+  //     res.redirect('/');
+  //   });
+  // });
 
   // Login page
   router.get('/login', (req, res) => {
